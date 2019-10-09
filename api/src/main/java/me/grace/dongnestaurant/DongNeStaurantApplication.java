aeb35a1e3325977045c110d4feb2a3d2ff7662ad
gracelove <govlmo91@gmail.com>
2019-10-09 15:03:32 +0900

    init

---
+++ api/src/main/java/me/grace/dongnestaurant/DongNeStaurantApplication.java
@@ -0,0 +1,13 @@
+package me.grace.dongnestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongNeStaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DongNeStaurantApplication.class, args);
+    }
+
+}
